a12d394d8ad535c87fdf926580d5e10076c98a63
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-08-17 23:10:04 -0300

    Se pone en home.blade.php la data de usuario, por ahora dummy (es decir, no leida desde una base de datos, sino hardcodeada

--- cw_laravel/resources/views/home.blade.php
+++ cw_laravel/resources/views/home.blade.php
@@ -2,25 +2,24 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    ¡Estás logueado!
-					<?php 
-					 
-					?>
-                </div>
+    <div class="row justify-content-center">		
+		@if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+                    <!--¡Estás logueado!-->
+		<div class='main-profile'>
+		<img class='profile-picture' style='margin-top: 10px;' height='200px' src='images/bill_gates.jpg' alt='Usuario'>
+		<h2 style='text-align:center;'>Bill Gates</h2>
+			<h3 style='text-align:center;'>
+				<p>Nombre</p>
+				<p>Usuario</p>
+				<p>País</p>
+				<p>Email</p>
+				<P>Tipo de usuario</p>
+			</h3>
+		</div>
     </div>
 </div>
 @endsection
